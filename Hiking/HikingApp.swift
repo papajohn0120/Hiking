7a15fdbcc98ad2b4427af93a07993c4cd839c190
Aaron Taylor <aarontaylor0120@gmail.com>
2024-11-29 21:30:49 -0600

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Aaron Taylor on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
